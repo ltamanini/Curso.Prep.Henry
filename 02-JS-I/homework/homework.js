b8380bc6c2492bb7e19a7dea704777e13255895d
Monchito1979 <ltamanini@outlook.com>
2021-07-07 13:49:59 +0200

    Agregado el homework y las explicaciones

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -246,12 +246,18 @@ function esVocal(letra) {
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
-    console.log('Es vocal')
-  } else {
-    console.log('Dato incorrecto')
+
+  if (letra.length > 1) {
+    return 'Dato incorrecto'
   }
 
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    return 'Es vocal'
+  }
+
+  if (letra !== 'a' || letra !== 'e' || letra !== 'i' || letra !== 'o' || letra !== 'u') {
+    return 'Dato incorrecto'
+  }
 }
 
 
